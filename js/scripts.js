2ab2c62b9e92827636eb97f980ceaf08ed449930
darkstars77 <darkstars77@naver.com>
2022-05-21 20:25:19 +0900

    1st draft

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,30 @@
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
+    // listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+const click_03 = document.querySelector('#click_03');
+const target_03 = document.querySelector('#target_03');
+
+click_03.addEventListener('click', ()=>{
+    target_03.scrollIntoView({block:'center'});
+});
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+window.initMap = initMap;
