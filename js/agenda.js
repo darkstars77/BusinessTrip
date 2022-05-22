e3ebad7c6fa266ee723e80df64207ca233f60d94
darkstars77 <darkstars77@naver.com>
2022-05-22 12:30:13 +0900

    font size & agenda download added

--- js/agenda.js
+++ js/agenda.js
@@ -1,32 +1,32 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    // listHoursArray[new Date().getDay()].classList.add(('today'));
-});
+// /*!
+// * Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
+// * Copyright 2013-2022 Start Bootstrap
+// * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+// */
+// // Highlights current date on contact page
+// window.addEventListener('DOMContentLoaded', event => {
+//     const listHoursArray = document.body.querySelectorAll('.list-hours li');
+//     // listHoursArray[new Date().getDay()].classList.add(('today'));
+// });
 
 
 
-const click_03 = document.querySelector('#click_03');
-const target_03 = document.querySelector('#target_03');
+// const click_03 = document.querySelector('#click_03');
+// const target_03 = document.querySelector('#target_03');
 
-const click_05 = document.querySelector('#click_05');
-const target_05 = document.querySelector('#target_05');
+// const click_05 = document.querySelector('#click_05');
+// const target_05 = document.querySelector('#target_05');
 
-const click_06 = document.querySelector('#click_06');
-const target_06 = document.querySelector('#target_06');
+// const click_06 = document.querySelector('#click_06');
+// const target_06 = document.querySelector('#target_06');a
 
-click_03.addEventListener('click', ()=>{
-    target_03.scrollIntoView({block:'start'});
-});
-click_05.addEventListener('click', ()=>{
-    target_05.scrollIntoView({block:'start'});
-});
-click_06.addEventListener('click', ()=>{
-    target_06.scrollIntoView({block:'start'});
-});
+// click_03.addEventListener('click', ()=>{
+//     target_03.scrollIntoView({block:'start'});
+// });
+// click_05.addEventListener('click', ()=>{
+//     target_05.scrollIntoView({block:'start'});
+// });
+// click_06.addEventListener('click', ()=>{
+//     target_06.scrollIntoView({block:'start'});
+// });
 
